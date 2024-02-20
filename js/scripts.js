41166a8ef9cc36f8b1909a828db5b21c0ecb7ca7
Vivian Davila <viviananddav@gmail.com>
2024-02-19 20:39:49 -0800

    Add html file, css and js content

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let userName = prompt("What's your name?");
+console.log("Question 1:", userName);
+
+let userAge = prompt("What's your age?");
+console.log("Question 2:", userAge);
+
+let favoriteColor = prompt("What's your favorite color?");
+console.log("Question 3:", favoriteColor);
+
+let learningSubject = prompt("What are you learning?");
+console.log("Question 4:", learningSubject);
